d3878b4afe0d7142622e4ce7dc1d38575a78e493
BitBasherr <betalik@outlook.com>
2025-08-01 04:51:36 -0500

    ghgh

--- custom_components/custom_device_notifier/evaluate.py
+++ custom_components/custom_device_notifier/evaluate.py
@@ -2,5 +2,5 @@ from homeassistant.helpers import condition
 
 
 def evaluate_condition(hass, cond: dict) -> bool:
-    checker = condition.from_config(cond, False)
+    checker = condition.from_config(cond)
     return checker(hass, {})
