68f68ab77dd7442b579d518309d7ad1b75702f9d
BitBasherr <betalik@outlook.com>
2025-08-02 15:50:52 -0500

    mypy structure

---
+++ custom_components/__init__.py
@@ -0,0 +1,5 @@
+"""Namespace package marker for Home Assistant custom components.
+
+This empty file turns `custom_components` into a regular package so
+mypy no longer thinks the inner integration is imported twice.
+"""
