6bc870b6a57e4067cdfe1cacc8c7cd96de3db618
BitBasherr <betalik@outlook.com>
2025-08-23 05:12:27 -0500

    Update __init__.py

--- custom_components/custom_device_notifier/__init__.py
+++ custom_components/custom_device_notifier/__init__.py
@@ -960,4 +960,3 @@ class PreviewManager:
             decision.update({"result": "dropped", "service_full": None})
 
         async_dispatcher_send(self.hass, _signal_name(self.entry.entry_id), decision)
-```0
